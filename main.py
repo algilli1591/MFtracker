1ad3cd29a1638393f400cebbeeca413dc3368b9e
Ainsley Gilligan <gilliganal@cardiff.ac.uk>
2019-06-04 21:58:05 +0100

    created main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+
+def csv_reader(csv):
+    """Read in CSV file"""
+    df =  pd.read_csv(csv)
+    return df
+
+
+def inventory_decrease(df):
+    """decreases inventory levels"""
+    
+
+
+
+def inventory_increase():
+    """increase inventory levels"""
+
